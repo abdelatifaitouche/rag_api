e4bd535c5e9bc09908a7ade7c094cc5803fe5718
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2025-02-16 15:59:28 +0100

    fixed the origin cors

--- working.py
+++ working.py
@@ -15,7 +15,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://gtdtt.digital"
 ]
 
 app.add_middleware(
