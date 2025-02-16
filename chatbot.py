3fd8d58a0551741e1781539399af9d3f4ff2582b
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2025-02-16 15:24:27 +0100

    add the rag func

--- chatbot.py
+++ chatbot.py
@@ -2,19 +2,45 @@ import google.generativeai as genai
 
 
 
+import chromadb
+import google.generativeai as genai
+from chromadb.utils import embedding_functions
 
 
-def chatbot(prompt:str):
+
+
+def chatbot(query_text):
 
     API_KEY = "AIzaSyD9tBP0NaJ6s-vA_qEOwbBtAMu34g1tw9c"
-
-    hello = ""
-
     genai.configure(api_key=API_KEY)
 
-    genai_model = genai.GenerativeModel('models/gemini-1.5-flash')
 
-    please = 'add'
-    response = genai_model.generate_content(prompt)
+    genai_model = genai.GenerativeModel('models/gemini-1.5-pro')
+
+    chroma_client = chromadb.PersistentClient('vector_db')
+
+    gemini_ef = embedding_functions.GoogleGenerativeAiEmbeddingFunction(api_key=API_KEY)
+
+
+    collection = chroma_client.get_collection(name='finance_gpt' , embedding_function=gemini_ef)
+
+    n_result = 5
+
+
+    results = collection.query(
+        query_texts=[query_text],
+        n_results=n_result,
+        include=['documents' , 'distances']
+    )
+
+    prompt = "en tant que consultant en finance, Repond a cette question en utilisant le Document comme context"
+    prompt += f'question : {query_text}'
+    prompt += f"Document : {results['documents']}"
+
+
+
+    response = genai_model.generate_content(prompt, stream=False)
 
     return response.text
+
+
---
+++ loader.py
@@ -0,0 +1,41 @@
+import google.generativeai as genai
+import chromadb
+from chromadb.utils import embedding_functions
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+
+
+API_KEY = "AIzaSyD9tBP0NaJ6s-vA_qEOwbBtAMu34g1tw9c"
+genai.configure(api_key=API_KEY)
+
+
+genai_model = genai.GenerativeModel('models/gemini-1.5-flash')
+
+chroma_client = chromadb.PersistentClient(path='vector_db')
+
+gemini_ef = embedding_functions.GoogleGenerativeAiEmbeddingFunction(api_key=API_KEY)
+
+chroma_collection = chroma_client.get_or_create_collection(name='finance_gpt' , embedding_function=gemini_ef)
+
+
+
+
+loader = PyPDFLoader('data.pdf')
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000)
+docs = text_splitter.split_documents(data) 
+document = []
+ids = []
+i = 1
+for doc in docs : 
+    ids.append(str(i))
+    i+=1
+    document.append(doc.page_content)
+
+
+chroma_collection.upsert(
+    documents=document,
+    ids=ids
+)
